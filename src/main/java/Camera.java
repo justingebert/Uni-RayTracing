063b874704afe6355b242d03c0692b338caf839b
Justin <justin.gebert@student.htw-berlin.de>
2023-05-02 18:07:40 +0200

    add pom.xml and basic Camera

--- src/main/java/Camera.java
+++ src/main/java/Camera.java
@@ -1,2 +1,13 @@
+import org.joml.Vector3;
+
 public class Camera {
+
+    private Vector3 position;
+    private Vector3 direction;
+    private Vector3 up;
+    private double fieldOfView;
+    private double aspectRatio;
+    private int imageWidth;
+    private int imageHeight;
+
 }
